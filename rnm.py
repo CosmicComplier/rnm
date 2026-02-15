37a043e964765bde35ba47ea961b02752872b09a
CosmicComplier <q13833989320@163.com>
2026-02-16 07:45:29 +0800

    我写完了耶耶耶

---
+++ rnm.py
@@ -0,0 +1,26 @@
+ddzWords = ["rnm()","调用日你妈的函数","准备日你妈"]
+class  people:#定义一个人员类
+    def __init__(self, name):
+        self.name = name
+        self.mum = True
+    def display(self):
+        print(f"Name: {self.name}")
+def fuck(person):
+    if person.mum == False:
+        print("不能日不存在的东西（")
+        return False
+    else:
+        print("验证通过！")
+        return True
+        
+def rnm():
+    name = input("请输入名字：")
+    person = people(name)
+    person.display()
+    if fuck(person) == True:
+        for words in ddzWords:
+            print(words)
+    else:
+        pass
+if __name__ == "__main__":
+    rnm()
